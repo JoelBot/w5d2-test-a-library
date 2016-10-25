0b1e1345687c8e4690096eaf98c1ee538e4bee43
JoelBot <Joel.Lonneker@gmail.com>
2016-10-25 16:51:42 -0400

    adventure completed

--- js/modom.js
+++ js/modom.js
@@ -1,73 +1,74 @@
 // Your DOM library code goes here inside of an IIFE and must set a global variables called md.
 (function () {
-    var one = function(selector) {
-        return document.querySelector(selector)
-    }
+   var one = function(selector) {
+      console.log(selector)
+      return document.querySelector(selector)
+   }
 
-    // var add = function(num1 = 0, num2 = 0, num3 = 0) {
-    //     return num1 + num2 + num3
-    // }
-    var add = function() {
-        var numbers = Array.from(arguments)
+   // var add = function(num1 = 0, num2 = 0, num3 = 0) {
+   //     return num1 + num2 + num3
+   // }
+   var add = function() {
+      var numbers = Array.from(arguments)
 
-        var total = numbers.reduce(function(previous, current) {
-            return previous + current
-        }, 0)
-        return total
-        // console.log(total)
-    }
+      var total = numbers.reduce(function(previous, current) {
+         return previous + current
+      }, 0)
+      return total
+      // console.log(total)
+   }
 
-    var all = function(selector) {
-        console.log(document.querySelectorAll(selector))
-        return document.querySelectorAll(selector)
-    } //: Find all matching elements using any CSS selector
+   var all = function(selector) {
+      console.log(document.querySelectorAll(selector))
+      return document.querySelectorAll(selector)
+   } //: Find all matching elements using any CSS selector
 
-    var remove = function(selector, index = null) {
-        // var SelectorArray = document.querySelectorAll(selector) // optional for FIND ALL
-        // var selector = document.querySelector(selector)
+   var remove = function(selector, index = null) {
+      // var SelectorArray = document.querySelectorAll(selector) // optional for FIND ALL
+      // var selector = document.querySelector(selector)
 
-        // SelectorArray.forEach(function(item, i){
-        // console.dir(item)
-        // selector.parentNode.removeChild(selector)
-        // })
-        // document.querySelectorAll(selector).parentNode.removeChild(selector)
-        // return 'Remove operation complete'
-        if (index === null) {
-            all(selector).remove() // Collins approach
-            console.log('removed: ' + selector)
-        }
-        else {
-            all(selector)[index].remove() // Collins approach
-            console.log('removed: ' + selector + ' number ' + index)
-        }
+      // SelectorArray.forEach(function(item, i){
+      // console.dir(item)
+      // selector.parentNode.removeChild(selector)
+      // })
+      // document.querySelectorAll(selector).parentNode.removeChild(selector)
+      // return 'Remove operation complete'
+      if (index === null) {
+         all(selector).remove() // Collins approach
+         console.log('removed: ' + selector)
+      }
+      else {
+         all(selector)[index].remove() // Collins approach
+         console.log('removed: ' + selector + ' number ' + index)
+      }
 
 
-    } //: Remove an element
-    var addClass = function(selector, classToAdd) {
-        // var collection = document.querySelector(selector)
-        // // collection.forEach(function(item) {
-        // collection.classList.add(classToAdd)
-        one(selector).classList.add(classToAdd)
-        // })
-        // return 'Class added to first specified tag'
-    }
-    var removeClass = function(selector, classToRemove) {
-        var collection = document.querySelector(selector)
-        // collection.forEach(function(item) {
-        collection.classList.remove(classToRemove)
-        // })
-        return 'Class removed to each tag found'
-    } //: Remove a CSS class from an element
-    var hasClass = function(selector, classCheck) {
-        // console.log("Output for hasClass function:")
-        // console.log(all(selector))
-        return one(selector).classList.contains(classCheck)
-        //     return true
-        // }
-        // else {
-        //     return false
-        // }
-    }//: Return true if an element contains a CSS class in it\'s CSS attribute (see classList documentation)
+   } //: Remove an element
+   var addClass = function(selector, classToAdd) {
+      // var collection = document.querySelector(selector)
+      // // collection.forEach(function(item) {
+      // collection.classList.add(classToAdd)
+      one(selector).classList.add(classToAdd)
+      // })
+      // return 'Class added to first specified tag'
+   }
+   var removeClass = function(selector, classToRemove) {
+      var collection = document.querySelector(selector)
+      // collection.forEach(function(item) {
+      collection.classList.remove(classToRemove)
+      // })
+      return 'Class removed to each tag found'
+   } //: Remove a CSS class from an element
+   var hasClass = function(selector, classCheck) {
+      // console.log("Output for hasClass function:")
+      // console.log(all(selector))
+      return one(selector).classList.contains(classCheck)
+      //     return true
+      // }
+      // else {
+      //     return false
+      // }
+   }//: Return true if an element contains a CSS class in it\'s CSS attribute (see classList documentation)
    //  var getAttr = function(selector, attribute){
    //      // var attributeVariable = document.querySelector(selector)
    //      // // console.log("Output for getAttr function:")
@@ -83,61 +84,69 @@
    //      return document.querySelector(selector).className = attribute
    //  } //: Set an HTML attribute on an element
    //
-    var getAttr = function (selector, attr) {
-       return one(selector).getAttribute(attr)
+   var getAttr = function(selector, attr) {
+      // console.log(one(selector).getAttribute(attr))
+      return one(selector).getAttribute(attr)
    }
 
    var setAttr = function (selector, attr, value) {
-       one(selector).setAttribute(attr, value)
-       return window.md
+      one(selector).setAttribute(attr, value)
+      return window.md
    }
-    var setHTML = function(selector, htmlToSet) {
-        return one(selector).innerHTML = htmlToSet
-    } //: Set the innerHTML of an element
-    var getHTML = function(selector) {
-        return document.querySelector(selector).innerHTML
-    } //: Get the innerHTML of an element
+   var setHTML = function(selector, htmlToSet) {
+      return one(selector).innerHTML = htmlToSet
+   } //: Set the innerHTML of an element
+   var getHTML = function(selector) {
+      return document.querySelector(selector).innerHTML
+   } //: Get the innerHTML of an element
 
-    //AJAX to make api call
-    var ajax = function(url, successCallback) {
-        fetch(url)
-        .then(res => res.json())
-        .then(successCallback)
-    }
+   var flipPage = function(value) {
+      // console.log(document.body.style)
+      setAttr('body','style',value)
+      // document.body.style.value = value
+   }
 
-    // Properties getProp
-    var getProp = function(selector, property) {
-        return one(selector)[property] // brackets to get around the literal object property issue that would be experienced if used as selector.property
-    }
+   //AJAX to make api call
+   var ajax = function(url, successCallback) {
+      fetch(url)
+      .then(res => res.json())
+      .then(successCallback)
+   }
 
-    var setProp = function(selector, property, value) {
-        one(selector)[property] = value// brackets to get around the literal object property issue that would be experienced if used as selector.property
-    }
-    var getValue = function(selector, value) {
-        return one(selector).value
-    }
-    var setValue = function(selector, value) {
-        return one(selector).value = value
-    }
+   // Properties getProp
+   var getProp = function(selector, property) {
+      return one(selector)[property] // brackets to get around the literal object property issue that would be experienced if used as selector.property
+   }
+
+   var setProp = function(selector, property, value) {
+      one(selector)[property] = value// brackets to get around the literal object property issue that would be experienced if used as selector.property
+   }
+   var getValue = function(selector, value) {
+      return one(selector).value
+   }
+   var setValue = function(selector, value) {
+      return one(selector).value = value
+   }
 
 
-    window.md = {
-        one: one,
-        add: add,
-        all: all,
-        remove: remove,
-        addClass: addClass,
-        removeClass: removeClass,
-        hasClass: hasClass,
-        getAttr: getAttr,
-        setAttr: setAttr,
-        setHTML: setHTML,
-        getHTML: getHTML,
-        ajax: ajax,
-        getProp: getProp,
-        setProp: setProp,
-        getValue: getValue,
-        setValue: setValue,
+   window.md = {
+      one: one,
+      add: add,
+      all: all,
+      remove: remove,
+      addClass: addClass,
+      removeClass: removeClass,
+      hasClass: hasClass,
+      getAttr: getAttr,
+      setAttr: setAttr,
+      setHTML: setHTML,
+      getHTML: getHTML,
+      ajax: ajax,
+      getProp: getProp,
+      setProp: setProp,
+      getValue: getValue,
+      setValue: setValue,
+      flipPage: flipPage
 
-    }
+   }
 }())
--- js/tests.js
+++ js/tests.js
@@ -7,10 +7,10 @@ describe('MoDom', function() {
       expect(md).to.exist
     })
   })
-  describe('md.addClass()', function() {
-    it('should add a CSS class', function() {
-      md.addClass('body', 'test')
-      expect(document.body.classList.contains('test')).to.be.true
+
+  describe('md.one()', function() {
+    it('should find at least one element', function() {
+      expect(md.one('table'))
     })
   })
   describe('md.all()', function() {
@@ -32,6 +32,10 @@ describe('MoDom', function() {
       md.addClass('body','placeholder')
       expect(document.body.classList.contains('placeholder'))
     })
+    it('should add a CSS class', function() {
+      md.addClass('body', 'test')
+      expect(document.body.classList.contains('test')).to.be.true
+    })
   })
   describe('md.removeClass()', function() {
     it('should remove test from body', function() {
@@ -50,9 +54,42 @@ describe('MoDom', function() {
     })
   })
   describe('md.getAttr()', function() {
-    it('should dislay table with X children', function() {
-      // md.removeClass('body','test')
-      expect(md.getAttr('table', 'children')).to.equal(7)
+    it('should validate image width to 200', function() {
+      expect(md.getAttr('img', 'width')).to.equal('200')
     })
   })
+  describe('md.setAttr()', function() {
+    it('should validate image width to 250', function() {
+      md.setAttr('img','width','250')
+      expect(md.getAttr('img', 'width')).to.equal('250')
+    })
+  })
+  describe('md.setHTML()', function() {
+    it('should validate h2 innerHTML to Big Old H2 Text.', function() {
+      md.setHTML('h2','Big Old H2 Text')
+      expect(md.getHTML('h2')).to.equal('Big Old H2 Text')
+    })
+  })
+  describe('md.getHTML()', function() {
+    it('should validate <a> innerHTML is ACME, Inc.', function() {
+      expect(md.getHTML('a')).to.equal('ACME, Inc.')
+    })
+  })
+  describe('md.flipPage()', function() {
+    it('should flip the page upside down', function() {
+      // md.setAttr('body','style','transform: rotate(180deg)')
+      md.flipPage('transform: rotate(180deg)')
+      expect(document.body.style.transform).to.equal('rotate(180deg)')
+      md.flipPage('transform: rotate(0deg)')
+    })
+  })
+
+  describe('md.ajax()', function() {
+    it('should connect to API and return without errors', function(done) {
+      expect(md.ajax('http://swapi.co/api/', function() {
+        done()
+      }))
+    });
+  });
+
 })
